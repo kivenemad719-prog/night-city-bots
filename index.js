99730abe1d280219f8a20a072d657aa57e773cc5
kivenemad719-prog <kivenemad719@gmail.com>
2026-03-25 03:13:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -990,6 +990,9 @@ client.once(Events.ClientReady, async () => {
   await sendTicketsPanel();
   await sendDecisionPanel();
   await sendDashboardPanel();
+
+  await testEmail(); // 👈 ده اللي ضفناه
+
   await sendLog('✅ تم تشغيل البوت.');
 });
 
